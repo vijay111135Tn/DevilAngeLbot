5fb0cd7f5db22090cb1f5a0a791f1e8b8f2a998d
GHOST #IU <74893002+Ghost-IU@users.noreply.github.com>
2021-04-15 21:08:19 +0530

    rm orfox | touch device

--- SaitamaRobot/modules/android.py
+++ SaitamaRobot/modules/android.py
@@ -46,65 +46,59 @@ def magisk(update, context):
 
 
 @typing_action
-def orangefox(update, context):
-    message = update.effective_message
-    chat = update.effective_chat
-    device = message.text[len("/orangefox ") :]
-    btn = ""
-
-    if device:
-        link = get(f"https://api.orangefox.download/v3/releases/?codename={device}&sort=date_desc&limit=1")
-
-        if link.status_code == 404:
-            msg = f"OrangeFox recovery is not avaliable for {device}"
-        else:
-            page = loads(link.content)
-            file_id = page["data"][0]["_id"]
-            link = get(f"https://api.orangefox.download/v3/devices/get?codename={device}")
-            page = loads(link.content)
-            oem = page["oem_name"]
-            model = page["model_name"]
-            full_name = page["full_name"]
-            maintainer = page["maintainer"]["username"]
-            link = get(f"https://api.orangefox.download/v3/releases/get?_id={file_id}")
-            page = loads(link.content)
-            dl_file = page["filename"]
-            build_type = page["type"]
-            version = page["version"]
-            changelog = page["changelog"][0]
-            size = str(round(float(page["size"]) / 1024 / 1024, 1)) + "MB"
-            dl_link = page["mirrors"]["DL"]
-            date = datetime.fromtimestamp(page["date"])
-            md5 = page["md5"]
-            msg = f"*Latest OrangeFox Recovery for the {full_name}*\n\n"
-            msg += f"• Manufacturer: `{oem}`\n"
-            msg += f"• Model: `{model}`\n"
-            msg += f"• Codename: `{device}`\n"
-            msg += f"• Build type: `{build_type}`\n"
-            msg += f"• Maintainer: `{maintainer}`\n"
-            msg += f"• Version: `{version}`\n"
-            msg += f"• Changelog: `{changelog}`\n"
-            msg += f"• Size: `{size}`\n"
-            msg += f"• Date: `{date}`\n"
-            msg += f"• File: `{dl_file}`\n"
-            msg += f"• MD5: `{md5}`\n"
-            btn = [[InlineKeyboardButton(text=f"Download", url = dl_link)]]
-    else:
-        msg = 'Give me something to fetch, like:\n`/orangefox a3y17lte`'
-
-    delmsg = message.reply_text(
-        text = msg,
-        reply_markup = InlineKeyboardMarkup(btn),
-        parse_mode = ParseMode.MARKDOWN,
-        disable_web_page_preview = True,
+def device(update, context):
+    args = context.args
+    if len(args) == 0:
+        reply = "No codename provided, write a codename for fetching informations."
+        del_msg = update.effective_message.reply_text(
+            "{}".format(reply),
+            parse_mode=ParseMode.MARKDOWN,
+            disable_web_page_preview=True,
+        )
+        time.sleep(5)
+        try:
+            del_msg.delete()
+            update.effective_message.delete()
+        except BadRequest as err:
+            if (err.message == "Message to delete not found") or (
+                err.message == "Message can't be deleted"
+            ):
+                return
+    device = " ".join(args)
+    db = get(DEVICES_DATA).json()
+    newdevice = device.strip("lte") if device.startswith("beyond") else device
+    try:
+        reply = f"Search results for {device}:\n\n"
+        brand = db[newdevice][0]["brand"]
+        name = db[newdevice][0]["name"]
+        model = db[newdevice][0]["model"]
+        codename = newdevice
+        reply += (
+            f"<b>{brand} {name}</b>\n"
+            f"Model: <code>{model}</code>\n"
+            f"Codename: <code>{codename}</code>\n\n"
+        )
+    except KeyError:
+        reply = f"Couldn't find info about {device}!\n"
+        del_msg = update.effective_message.reply_text(
+            "{}".format(reply),
+            parse_mode=ParseMode.MARKDOWN,
+            disable_web_page_preview=True,
+        )
+        time.sleep(5)
+        try:
+            del_msg.delete()
+            update.effective_message.delete()
+        except BadRequest as err:
+            if (err.message == "Message to delete not found") or (
+                err.message == "Message can't be deleted"
+            ):
+                return
+    update.message.reply_text(
+        "{}".format(reply), parse_mode=ParseMode.HTML, disable_web_page_preview=True
     )
-
-    cleartime = get_clearcmd(chat.id, "orangefox")
-
-    if cleartime:
-        context.dispatcher.run_async(delete, delmsg, cleartime.time)
-        
-        
+ 
+       
 @typing_action
 def twrp(update, context):
     args = context.args
@@ -175,6 +169,7 @@ __help__ = """
 Get the latest Magsik releases or TWRP for your device!
 *Android related commands:*
  × /magisk - Gets the latest magisk release for Stable/Beta/Canary.
+ × /device <codename> - Gets android device basic info from its codename.
  × /twrp <codename> -  Gets latest twrp for the android device using the codename.
 """
 
@@ -182,8 +177,8 @@ __mod_name__ = "Android"
 
 MAGISK_HANDLER = DisableAbleCommandHandler("magisk", magisk)
 TWRP_HANDLER = DisableAbleCommandHandler("twrp", twrp, pass_args=True)
-ORANGEFOX_HANDLER = DisableAbleCommandHandler("orangefox", orangefox, pass_args=True)
+DEVICE_HANDLER = DisableAbleCommandHandler("device", device, pass_args=True)
 
 dispatcher.add_handler(MAGISK_HANDLER)
 dispatcher.add_handler(TWRP_HANDLER)
-dispatcher.add_handler(ORANGEFOX_HANDLER)
+dispatcher.add_handler(DEVICE_HANDLER)
