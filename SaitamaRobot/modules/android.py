6a702753b5ce1a0b5f532ace237623a8559e2259
GHOST #IU <74893002+Ghost-IU@users.noreply.github.com>
2021-04-19 22:44:56 +0530

    Update android.py

--- SaitamaRobot/modules/android.py
+++ SaitamaRobot/modules/android.py
@@ -233,7 +233,7 @@ __mod_name__ = "Android"
 MAGISK_HANDLER = DisableAbleCommandHandler("magisk", magisk)
 TWRP_HANDLER = DisableAbleCommandHandler("twrp", twrp, pass_args=True)
 DEVICE_HANDLER = DisableAbleCommandHandler("device", device, pass_args=True)
-ORANGEFOX_HANDLER = DisableAbleCommandHandler("orfox", orangefox, pass_args=True)
+ORANGEFOX_HANDLER = DisableAbleCommandHandler("orangefox", orangefox, pass_args=True)
 
 dispatcher.add_handler(MAGISK_HANDLER)
 dispatcher.add_handler(TWRP_HANDLER)
