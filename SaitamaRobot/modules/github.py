6dbdda05f34c02add77e2c93d703d4ca8dd02220
Restyled.io <commits@restyled.io>
2021-05-01 07:14:16 +0000

    Restyled by black

--- SaitamaRobot/modules/github.py
+++ SaitamaRobot/modules/github.py
@@ -50,8 +50,7 @@ def repo(update, context):
         )
     elif len(args) == 1:
         user = args[0]
-        usr_data = get(
-            f"https://api.github.com/users/{user}/repos?per_page=40").json()
+        usr_data = get(f"https://api.github.com/users/{user}/repos?per_page=40").json()
 
         if len(usr_data) != 0:
             reply_text = f"*{user}*" + f"'s" + "* Repos:*\n"
@@ -70,8 +69,7 @@ def repo(update, context):
     else:
         user, repo = args
         rep_data = get(f"https://api.github.com/repos/{user}/{repo}").json()
-        brc_data = get(
-            f"https://api.github.com/repos/{user}/{repo}/branches").json()
+        brc_data = get(f"https://api.github.com/repos/{user}/{repo}/branches").json()
         try:
             text = f"*Repo name:* {rep_data['full_name']}"
             text += f"\n*Language*: {rep_data['language']}"
