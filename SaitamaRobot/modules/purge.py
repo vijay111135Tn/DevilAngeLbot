0eaed425b92c2f43ec983907b4652426d3191c51
Restyled.io <commits@restyled.io>
2021-05-01 05:34:54 +0000

    Restyled by yapf

--- SaitamaRobot/modules/purge.py
+++ SaitamaRobot/modules/purge.py
@@ -12,13 +12,10 @@ async def purge_messages(event):
     if event.from_id is None:
         return
 
-    if (
-        not await user_is_admin(
+    if (not await user_is_admin(
             user_id=event.sender_id,
             message=event,
-        )
-        and event.from_id not in [1087968824]
-    ):
+    ) and event.from_id not in [1087968824]):
         await event.reply("Only Admins are allowed to use this command")
         return
 
@@ -28,7 +25,8 @@ async def purge_messages(event):
 
     reply_msg = await event.get_reply_message()
     if not reply_msg:
-        await event.reply("Reply to a message to select where to start purging from.")
+        await event.reply(
+            "Reply to a message to select where to start purging from.")
         return
     messages = []
     message_id = reply_msg.id
@@ -54,13 +52,10 @@ async def delete_messages(event):
     if event.from_id is None:
         return
 
-    if (
-        not await user_is_admin(
+    if (not await user_is_admin(
             user_id=event.sender_id,
             message=event,
-        )
-        and event.from_id not in [1087968824]
-    ):
+    ) and event.from_id not in [1087968824]):
         await event.reply("Only Admins are allowed to use this command")
         return
 
