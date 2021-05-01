9518e2a800c14d8225ed8de01ee41fed6670a65e
Restyled.io <commits@restyled.io>
2021-05-01 05:34:51 +0000

    Restyled by isort

--- SaitamaRobot/modules/purge.py
+++ SaitamaRobot/modules/purge.py
@@ -1,11 +1,10 @@
 import time
+
 from telethon import events
 
 from SaitamaRobot import telethn
 from SaitamaRobot.modules.helper_funcs.telethn.chatstatus import (
-    can_delete_messages,
-    user_is_admin,
-)
+    can_delete_messages, user_is_admin)
 
 
 async def purge_messages(event):
