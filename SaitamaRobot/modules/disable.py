adb2f097bf428cb6e29f2ed44dfc103db3863790
GHOST #IU <74893002+Ghost-IU@users.noreply.github.com>
2021-06-18 22:09:16 +0530

    Shorten module name

--- SaitamaRobot/modules/disable.py
+++ SaitamaRobot/modules/disable.py
@@ -350,7 +350,7 @@ if is_module_loaded(FILENAME):
     • `/listcmds`*:* list all possible toggleable commands
     """
 
-    __mod_name__ = "Disable Module"
+    __mod_name__ = "Disabling"
 
 else:
     DisableAbleCommandHandler = CommandHandler
